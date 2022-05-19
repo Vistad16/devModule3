d9c115f0a77c56ac46ab5fc776c4f1848854aed2
Bohdan Datsiv <bohdan.datsiv@gmail.com>
2022-05-19 15:58:13 +0300

    add 3.2 hw 1.sql

---
+++ src/main/java/1.sql
@@ -0,0 +1,9 @@
+ALTER TABLE developer
+ADD salary INT;
+
+UPDATE developer SET salary = 10000 WHERE id = 4;
+UPDATE developer SET salary = 20 WHERE id = 5;
+UPDATE developer SET salary = 6700 WHERE id = 6;
+UPDATE developer SET salary = 5200 WHERE id = 7;
+UPDATE developer SET salary = 7000 WHERE id = 8;
+UPDATE developer SET salary = 12500 WHERE id = 9;
