fa381601d475dc235263530691928f80dbcb2ac6
Bohdan Datsiv <bohdan.datsiv@gmail.com>
2022-05-21 11:41:01 +0300

    refactor 5.sql

--- src/main/java/5.sql
+++ src/main/java/5.sql
@@ -1,4 +1,9 @@
-SELECT ID, PROJECTS_NAME, DESCRIPTION, COST
+SELECT id, projects_name, description, cost
 FROM projects
-GROUP BY cost
+ORDER BY cost, id ASC
 LIMIT 1;
+
+--aggregation
+SELECT *
+FROM projects
+WHERE cost = (SELECT min(cost) FROM projects);
